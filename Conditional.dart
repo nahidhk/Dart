b5c582e5222f2d2e62fcd97470ea8bae3a48acc3
nahidhk <nahidappltd@gmail.com>
2024-09-06 11:35:40 +0600

    new update

---
+++ Conditional.dart
@@ -0,0 +1,27 @@
+
+// Page day 2
+void main(){
+  var connection = 'connected';
+  if (connection == "connected") {
+    print("Connected");
+  } else if(connection == "waining"){
+     print("waiting");
+  }
+   else {
+    print("Disconnected");
+  }
+
+var a = 'Hello';
+var b = "i'm";
+var c = 'Dart';
+var d = 'developer';
+
+print(a+b+c+d);
+print("$a$b$c$d Commend in ok");
+ List <String> f = [a,b,c,d];
+ print(f.join()) ;
+
+
+
+
+}
--- main.dart
+++ main.dart
@@ -10,32 +10,45 @@ void main() {
   double cba = 1;
   print(abc);
   print(cba);
-//   String Data Types
+  //String Data Types
   String name = "Nahid HK";
   print(name);
-//   bollin Data Types
+  //bollin Data Types
   bool velues = true;
   print(velues);
-// list Datatype
+  //list Datatype
   List mylist = ['one', 'tow', 'three', 'fore'];
   print(mylist);
-//map datatype
-  Map <String, dynamic> mymap = 
-  {
-    "name": "nahid hk", "Age": 17, "student": true
-  };
+  //map datatype
+  Map<String, dynamic> mymap = {"name": "nahid hk", "Age": 17, "student": true};
   print(mymap);
-  // Set Data Type
-
-  Set myset = {
-1,"two",3,true
-  };
+  //Set Data Type
+  Set myset = {1, "two", 3, true};
   print(myset);
   // Runes
   final myname = "nahid";
   print(myname.codeUnits);
   print(myname.hashCode);
-
-  Runes inputs =Runes('\u{1F49A}');
+  // The Runs Icon Show
+  Runes inputs = Runes('\u{1F49A}');
   print(String.fromCharCodes(inputs));
+  var x = 4;
+  var y =3.9;
+  var reslut = x*y > x/y;
+  var intu = x*y;
+  var dibaided = x/y;
+  var mainas = x-y;
+  var plush = x+y;
+  print(reslut);
+  print(intu);
+  print(dibaided);
+  print(plush);
+  print(mainas);
+  var color = "red";
+  var ifshow = color=="red"?"Color Is Red":"No Color";
+  print(ifshow);
+  int? myge;
+  var vinti = myge??17;
+  print(vinti);
+ 
 }
