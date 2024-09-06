5d663a0fcbcac7d66e5a474b6f6dff76266c23dd
nahidhk <nahidappltd@gmail.com>
2024-09-06 09:05:45 +0600

    new update

---
+++ main.dart
@@ -0,0 +1,41 @@
+void main() {
+  // Dart ne w print
+  print("Hello word \n Nahid Hk \r developr");
+  // All Data Types
+  // Dart integar
+  int age = 17;
+  print(age);
+  // double Data types
+  double abc = 2.5;
+  double cba = 1;
+  print(abc);
+  print(cba);
+//   String Data Types
+  String name = "Nahid HK";
+  print(name);
+//   bollin Data Types
+  bool velues = true;
+  print(velues);
+// list Datatype
+  List mylist = ['one', 'tow', 'three', 'fore'];
+  print(mylist);
+//map datatype
+  Map <String, dynamic> mymap = 
+  {
+    "name": "nahid hk", "Age": 17, "student": true
+  };
+  print(mymap);
+  // Set Data Type
+
+  Set myset = {
+1,"two",3,true
+  };
+  print(myset);
+  // Runes
+  final myname = "nahid";
+  print(myname.codeUnits);
+  print(myname.hashCode);
+
+  Runes inputs =Runes('\u{1F49A}');
+  print(String.fromCharCodes(inputs));
+}
